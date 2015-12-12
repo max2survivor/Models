8434f5f6be2120c2ef5cb882a5b5847321881833
ould <ould@ould>
2015-12-12 20:59:21 +0100

    commit

---
+++ src/core/LanguageModel.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package core;
+
+/**
+ * @author ould
+ *
+ */
+public class LanguageModel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
